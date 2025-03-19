4e1366d637863564c7269c587b55f527d33be45d
Franco Rodriguez <franco@calim.com.ar>
2025-03-19 16:13:09 -0300

    Me faltaban estos push

---
+++ ejercicios/aceleracion/ej1.py
@@ -0,0 +1,4 @@
+import math
+
+def funcion (x):
+    return (math.pi/2)*(x**2)-x-2
---
+++ ejercicios/newton/ej1.py
@@ -0,0 +1,19 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+from ejercicios.metodos_numericos import newtonRaphson
+
+
+def funcion(x):
+    return (x-1)**2
+
+def derivada(x):
+    return 2*(x-1)
+
+x0 = 0
+tolerancia = 1e-6
+print(f"\nMétodo de Newton-Raphson:")
+resultado, iteraciones = newtonRaphson(funcion, derivada, x0, tolerancia)
+print(f"\nResultado: {resultado}")
+print(f"Iteraciones: {iteraciones}")
+
