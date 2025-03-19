b8562324a0a2047848d7b9d108f3458dc29e4472
Franco Rodriguez <franco@calim.com.ar>
2025-03-18 21:16:24 -0300

    Aclaro el proceso

--- ejercicios/puntoFijo/ej4.py
+++ ejercicios/puntoFijo/ej4.py
@@ -3,11 +3,27 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 from ejercicios.metodos_numericos import puntoFijo
 
+x0 = 1 # Lo dice el enunciado
+# f(x) = x**3 - x - 1
+# 0 = x**3 - x - 1
+# x = x**3 - 1 = g(x)
+# g'(x) = 3x**2 - 1
+# g'(x0) = 3*1**2 - 1 = 2
+# |g'(x0)| = 2 > 1
+# No converge
+# reescribo g(x)
+# x = x**3 - 1
+# x = (x + 1)**(1/3) = g(x)
+# g'(x) = 1/3 * (x + 1)**(-2/3)
+# g'(x0) = 1/3 * (1 + 1)**(-2/3) = 1/3 * 2**(-2/3) = 1/3 * 1/2**(2/3) = 1/3 * 1/1.5874 = 0.2
+# |g'(x0)| = 0.2 < 1
+# Converge
+
+
 def funcion(x):
     return (x+1)**(1/3)
 
 def main():
-    x0 = 1
     tolerancia = 1e-6
     print(f"\nMétodo Punto Fijo:")
     resultado, iteraciones = puntoFijo(funcion, x0, tolerancia)
